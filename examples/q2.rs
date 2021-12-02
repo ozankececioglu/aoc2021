6d53d208f0cefbfc499209b0ecfa297213743375
ozan kececioglu <ozankececioglu@users.noreply.github.com>
2021-12-02 13:17:25 +0100

    q2.1

---
+++ examples/q2.rs
@@ -0,0 +1,35 @@
+use std::{fs::File};
+
+use std::io::{self, prelude::*, BufReader};
+
+use std::{iter};
+use std::collections::HashSet;
+use std::collections::VecDeque;
+use regex::{Regex};
+
+
+fn main() -> io::Result<()> {
+    let file = File::open("data/q2")?;
+    let reader = BufReader::new(file);
+
+    let mut dist = 0;
+    let mut depth = 0;
+
+    for line in reader.lines().map(|l| l.unwrap()) {
+        let mut fields = line.split_whitespace();
+        let command = fields.next().unwrap();
+        let arg = fields.next().unwrap().parse::<i32>().unwrap();
+        match command {
+            "forward" => dist += arg,
+            "down" => depth += arg,
+            "up" => depth -= arg,
+            _ => ()
+        }
+    }
+
+    println!("!### {} {} {}", dist, depth, dist * depth);
+
+
+
+    Ok(())
+}
